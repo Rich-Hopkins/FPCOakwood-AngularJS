d21e662aa4cac8fb732c13688f605957f0e3a9b3
Richard Hopkins <hopmedic@hotmail.com>
2018-06-11 15:04:56 -0400

    Starting on Gallery

---
+++ FPC.WebApi/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Configuration;
+using System.Web.Http;
+
+namespace FPC.WebApi.Controllers
+{
+    public class GalleryController : ApiController
+    {
+        private string flickrKey = WebConfigurationManager.AppSettings["FlickrKey"];
+        private string flickrSecret = WebConfigurationManager.AppSettings["FlickrSecret"];
+        private string flickrUser = WebConfigurationManager.AppSettings["FlickrUserID"];
+
+        public string Get()
+        {
+            return "The service is working.";
+        }
+    }
+}
--- FPC.WebApi/Models/Event.cs
+++ FPC.WebApi/Models/Event.cs
@@ -5,16 +5,16 @@ using System.Web;
 
 namespace FPC.WebApi.Models
 {
-	public class Event
-	{
-		public string Id { get; set; }
-		public string Title { get; set; }
-		public string ShortDesc { get; set; }
-    public string LongDesc { get; set; }
-    public string ImageName { get; set; }
-    public string TemplateUrl { get; set; }
-		public DateTime DisplayDate { get; set; }
-    public DateTime StartDate { get; set; }
-		public DateTime EndDate { get; set; }
-	}
+    public class Event
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDesc { get; set; }
+        public string LongDesc { get; set; }
+        public string ImageName { get; set; }
+        public string TemplateUrl { get; set; }
+        public DateTime DisplayDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
 }
