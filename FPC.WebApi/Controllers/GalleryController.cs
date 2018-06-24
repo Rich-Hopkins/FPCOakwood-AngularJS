e2171f5560c1f4c63ab761e349b3e4fd0a40a60b
Richard Hopkins <hopmedic@hotmail.com>
2018-06-23 20:10:10 -0400

    Gallery API working, merged to Master

--- FPC.WebApi/Controllers/GalleryController.cs
+++ FPC.WebApi/Controllers/GalleryController.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Web.Configuration;
 using System.Web.Http;
-using FPC.WebApi.Models;
 using FlickrNet;
 using Photo = FPC.WebApi.Models.Photo;
 using Photoset = FPC.WebApi.Models.Photoset;
