a6fa958a25771ecd433c104d40ae287c69bb4c93
Richard Hopkins <hopmedic@hotmail.com>
2018-08-08 16:42:48 -0400

    Early in gallery

--- FPC.WebApi/Controllers/EventsController.cs
+++ FPC.WebApi/Controllers/EventsController.cs
@@ -11,48 +11,48 @@ using FPC.WebApi.Models;
 
 namespace FPC.WebApi.Controllers
 {
-	public class EventsController : ApiController
-	{
-		public IEnumerable<Event> GetAllEvents()
-		{
-			List<Event> events = new List<Event>();
-			string path = HttpRuntime.AppDomainAppPath + "App_Data/eventdata.txt";
-			int id = 0;
-			string line;
-			try
-			{
-				using (StreamReader file = new StreamReader(path))
-				{
-          //skip first 8 lines that guide remainder of file
-          for(var i = 0; i < 8; i++)
-          {
-            file.ReadLine();
-          }
-
-					while ((line = file.ReadLine()) != null)
-					{
-            events.Add(new Event()
+    public class EventsController : ApiController
+    {
+        public IEnumerable<Event> GetAllEvents()
+        {
+            List<Event> events = new List<Event>();
+            string path = HttpRuntime.AppDomainAppPath + "App_Data/eventdata.txt";
+            int id = 0;
+            string line;
+            try
             {
-              Id = "event" + id,
-              Title = line.Trim(),
-              ShortDesc = file.ReadLine().Trim(),
-              LongDesc = file.ReadLine().Trim(),
-              ImageName = file.ReadLine().Trim(),
-              TemplateUrl = file.ReadLine().Trim(),
-              DisplayDate = DateTime.Parse(file.ReadLine().Trim()),
-              StartDate = DateTime.Parse(file.ReadLine().Trim()),
-              EndDate = DateTime.Parse(file.ReadLine().Trim())
-            });
-						id++;
-					}
-				};
-			}
-			catch (Exception ex)
-			{
-				
-			}
+                using (StreamReader file = new StreamReader(path))
+                {
+                    //skip first 8 lines that guide remainder of file
+                    for (var i = 0; i < 8; i++)
+                    {
+                        file.ReadLine();
+                    }
 
-      return events.Where(e => e.EndDate > DateTime.Today.AddDays(-1) && e.DisplayDate <= DateTime.Today);
-		}
-	}
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        events.Add(new Event()
+                        {
+                            Id = "event" + id,
+                            Title = line.Trim(),
+                            ShortDesc = file.ReadLine().Trim(),
+                            LongDesc = file.ReadLine().Trim(),
+                            ImageName = file.ReadLine().Trim(),
+                            TemplateUrl = file.ReadLine().Trim(),
+                            DisplayDate = DateTime.Parse(file.ReadLine().Trim()),
+                            StartDate = DateTime.Parse(file.ReadLine().Trim()),
+                            EndDate = DateTime.Parse(file.ReadLine().Trim())
+                        });
+                        id++;
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return events.Where(e => e.EndDate > DateTime.Today.AddDays(-1) && e.DisplayDate <= DateTime.Today);
+        }
+    }
 }
--- FPC.WebApi/Controllers/GalleryController.cs
+++ FPC.WebApi/Controllers/GalleryController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Configuration;
 using System.Web.Http;
 using FlickrNet;
@@ -19,40 +20,46 @@ namespace FPC.WebApi.Controllers
             flickrKey = WebConfigurationManager.AppSettings["FlickrKey"];
             flickrUser = WebConfigurationManager.AppSettings["FlickrUserID"];
             flickr = new Flickr(flickrKey);
-
-            var photosetCollection = flickr.PhotosetsGetList(flickrUser);
-            var photosets = new List<Photoset>();
-            foreach (var photoset in photosetCollection)
+            try
             {
-                var ps = new Photoset
+                var photosetCollection = flickr.PhotosetsGetList(flickrUser);
+                var photosets = new List<Photoset>();
+                foreach (var photoset in photosetCollection)
                 {
-                    Title = photoset.Title,
-                    Id = photoset.PhotosetId,
-                    Description = photoset.Description,
-                    Photos = new List<Photo>()
-                };
-                photosets.Add(ps);
-            }
-
-            foreach (var photoset in photosets)
-            {
-                var flickrPhotos = flickr.PhotosetsGetPhotos(photoset.Id);
-                foreach (var flickrPhoto in flickrPhotos)
-                {
-                    var p = new Photo
+                    var ps = new Photoset
                     {
-                        Id = flickrPhoto.PhotoId,
-                        Title = flickrPhoto.Title,
-                        Description = flickrPhoto.Description,
-                        Thumbnail = flickrPhoto.ThumbnailUrl,
-                        Small = flickrPhoto.SmallUrl,
-                        Medium = flickrPhoto.MediumUrl,
-                        Large = flickrPhoto.LargeUrl
+                        Title = photoset.Title,
+                        Id = photoset.PhotosetId,
+                        Description = photoset.Description,
+                        Photos = new List<Photo>()
                     };
-                    photoset.Photos.Add(p);
+                    photosets.Add(ps);
                 }
+
+                foreach (var photoset in photosets)
+                {
+                    var flickrPhotos = flickr.PhotosetsGetPhotos(photoset.Id);
+                    foreach (var flickrPhoto in flickrPhotos)
+                    {
+                        var p = new Photo
+                        {
+                            Id = flickrPhoto.PhotoId,
+                            Title = flickrPhoto.Title,
+                            Description = flickrPhoto.Description,
+                            Thumbnail = flickrPhoto.ThumbnailUrl,
+                            Small = flickrPhoto.SmallUrl,
+                            Medium = flickrPhoto.MediumUrl,
+                            Large = flickrPhoto.LargeUrl
+                        };
+                        photoset.Photos.Add(p);
+                    }
+                }
+                return photosets;
             }
-            return photosets;
+            catch (Exception)
+            {
+                return null;
+            }
         }
     }
 }
