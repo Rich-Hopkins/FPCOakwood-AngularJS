038f56ff0033f0b95122c33744e53c063390dd9d
Richard Hopkins <hopmedic@hotmail.com>
2018-08-27 14:29:52 -0400

    Imgur integration started

---
+++ FPC.WebApi/Controllers/ImgurController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Configuration;
+using System.Web.Http;
+using FPC.WebApi.Models;
+using Imgur.API;
+using Imgur.API.Authentication.Impl;
+
+namespace FPC.WebApi.Controllers
+{
+    public class ImgurController : ApiController
+    {
+        private string imgurKey;
+        private string imgurSecret;
+        private ImgurClient imgur;
+
+        public IEnumerable<Photoset> Get()
+        {
+            imgurKey = WebConfigurationManager.AppSettings["ImgurKey"];
+            imgurSecret = WebConfigurationManager.AppSettings["ImgurSecret"];
+            imgur = new ImgurClient(imgurKey, imgurSecret);
+
+
+
+            return Void;
+        }
+    }
+}
