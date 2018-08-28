8614c371863a348324f8d71f62910834b447ee83
Richard Hopkins <hopmedic@hotmail.com>
2018-08-28 00:38:57 -0400

    Working with Imgur

--- FPC.WebApi/Controllers/ImgurController.cs
+++ FPC.WebApi/Controllers/ImgurController.cs
@@ -1,57 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Configuration;
 using System.Web.Http;
 using FPC.WebApi.Models;
-using Imgur.API;
-using Imgur.API.Authentication.Impl;
-using Imgur.API.Endpoints.Impl;
-using Imgur.API.Models.Impl;
 
 namespace FPC.WebApi.Controllers
 {
     public class ImgurController : ApiController
     {
 
-        public async System.Threading.Tasks.Task<IEnumerable<Photoset>> GetAsync()
+        public object Get()
         {
 
 
             try
             {
                 var imgurClientId = WebConfigurationManager.AppSettings["ImgurClientId"];
-                var imgurSecret = WebConfigurationManager.AppSettings["ImgurSecret"];
-                var imgurClient = new ImgurClient(imgurClientId);
-                
-                var accountEndpoint = new AccountEndpoint(imgurClient);
-                var albumEndpoint = new AlbumEndpoint(imgurClient);
-                var imageEndpoint = new ImageEndpoint(imgurClient);
-                var photosets = new List<Photoset>();
-                var x = new Imgur.API.Models
-                var albums = 
-
-                foreach (var album in albums)
+                using (var client = new HttpClient())
                 {
-                    var ps = new Photoset
-                    {
-                        Title = album.Title,
-                        Id = album.Id,
-                        Description = album.Description,
-                        Photos = new List<Photo>()
-                    };
-                    photosets.Add(ps);
+                    client.BaseAddress = new Uri("https://api.imgur.com/3/");
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Client-ID", "2d5c32e8beb66e4");
+                    var response = client.GetStringAsync("account/fpcoakwood/albums").Result;
+                    return response;
                 }
-
-                //foreach(var photoset in photosets)
-                //{
-                //    int slideId = 0;
-                //    var 
-                //}
-
-                return photosets;
             }
             catch
             {
