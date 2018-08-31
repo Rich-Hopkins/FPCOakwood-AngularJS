5be7e31184317fb97bf3e500743bd3444079c953
Richard Hopkins <hopmedic@hotmail.com>
2018-08-31 16:58:00 -0400

    Imgur API returning info

--- FPC.WebApi/Controllers/ImgurController.cs
+++ FPC.WebApi/Controllers/ImgurController.cs
@@ -6,13 +6,14 @@ using System.Web.Configuration;
 using System.Web.Http;
 using System.Web.Script.Serialization;
 using FPC.WebApi.Models.Albums;
+using FPC.WebApi.Models.Images;
 
 namespace FPC.WebApi.Controllers
 {
     public class ImgurController : ApiController
     {
 
-        public List<Datum> Get()
+        public List<Album> Get()
         {
 
 
@@ -23,8 +24,15 @@ namespace FPC.WebApi.Controllers
                 {
                     client.BaseAddress = new Uri("https://api.imgur.com/3/");
                     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Client-ID", "2d5c32e8beb66e4");
-                    var response = client.GetStringAsync("account/fpcoakwood/albums").Result;
-                    var albums = new JavaScriptSerializer().Deserialize<Albums>(response);
+                    var albumResponse = client.GetStringAsync("account/fpcoakwood/albums").Result;
+                    var albums = new JavaScriptSerializer().Deserialize<Albums>(albumResponse);
+
+                    foreach(var album in albums.Data)
+                    {
+                        var imageResponse = client.GetStringAsync(string.Format("album/{0}/images", album.Id)).Result;
+                        var images = new JavaScriptSerializer().Deserialize<Images>(imageResponse).Data;
+                        album.Images = images;
+                    }
                     return albums.Data;
                 }
             }
--- FPC.WebApi/Models/Imgur.cs
+++ FPC.WebApi/Models/Imgur.cs
@@ -7,16 +7,17 @@ namespace FPC.WebApi.Models.Albums
 {
     public class Albums
     {
-        public List<Datum> Data { get; set; }
+        public List<Album> Data { get; set; }
         //public bool Success { get; set; }
         //public long Status { get; set; }
     }
 
-    public class Datum
+    public class Album
     {
         public string Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public List<Images.Image> Images {get;set;}
         //public long Datetime { get; set; }
         //public string Cover { get; set; }
         //public long CoverWidth { get; set; }
@@ -41,5 +42,40 @@ namespace FPC.WebApi.Models.Albums
 
 namespace FPC.WebApi.Models.Images
 {
+    public class Images
+    {
+        public List<Image> Data { get; set; }
+        public bool Success { get; set; }
+        public long Status { get; set; }
+    }
+
+    public class Image
+    {
+        public string Id { get; set; }
+        public object Title { get; set; }
+        public string Description { get; set; }
+        public long Datetime { get; set; }
+        public string Type { get; set; }
+        public bool Animated { get; set; }
+        public long Width { get; set; }
+        public long Height { get; set; }
+        public long Size { get; set; }
+        public long Views { get; set; }
+        public long Bandwidth { get; set; }
+        public object Vote { get; set; }
+        public bool Favorite { get; set; }
+        public object Nsfw { get; set; }
+        public object Section { get; set; }
+        public object AccountUrl { get; set; }
+        public object AccountId { get; set; }
+        public bool IsAd { get; set; }
+        public bool InMostViral { get; set; }
+        public bool HasSound { get; set; }
+        public List<object> Tags { get; set; }
+        public long AdType { get; set; }
+        public string AdUrl { get; set; }
+        public bool InGallery { get; set; }
+        public string Link { get; set; }
+    }
 
 }
