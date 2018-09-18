7b6598268300e52575b4fa04e407fc700c4b46b2
Richard Hopkins <hopmedic@hotmail.com>
2018-09-18 07:12:57 -0400

    More on animation.

--- FPC.WebApi/views/gallery/GalleryController.js
+++ FPC.WebApi/views/gallery/GalleryController.js
@@ -13,31 +13,18 @@
                 vm.galleryList = data;
                 vm.selectedGallery = vm.galleryList[0];
                 vm.slides = vm.selectedGallery.Images;
-                vm.index = 0;
+                vm.index = -1;
             },
                 function (error) {
                     console.log('Error: ' + error);
                 });
-
-        vm.showPicture = function () {
-            vm.slides[vm.index].Hidden = false;
-        };
-
-        vm.hideAll = function () {
-            for (var i = 0; i < vm.slides.length; i++) {
-                vm.slides[i].Hidden = true;
-            }
-        };
-
+        
         vm.imageClicked = function (index) {
             vm.index = index;
-            vm.hideAll();
-            vm.showPicture();
         };
 
         vm.plusSlides = function (n) {
             var i = vm.index + n;
-            vm.slides[vm.index].Hidden = true;
             if (i > vm.slides.length - 1) {
                 i = 0;
             }
@@ -45,21 +32,14 @@
                 i = vm.slides.length - 1;
             }
             vm.index = i;
-            vm.showPicture();
             console.log(vm.index);
             console.log(vm.slides);
         };
 
         vm.changeGallery = function () {
             vm.slides = vm.selectedGallery.Images;
-            vm.hideAll();
-            vm.index = 0;
+            vm.index = -1;
         };
-
-        vm.animate = function () {
-            vm.hidden = !vm.hidden;
-        };
-
         return vm;
     }
 })();
